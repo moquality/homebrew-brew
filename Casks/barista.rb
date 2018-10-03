1235ced76ae2f78a3f07961ee8c73e120a2d1e60
Pushkar Kolhe <pushkar@moquality.com>
2018-10-02 21:33:28 -0400

    Updates barista cask

--- Casks/barista.rb
+++ Casks/barista.rb
@@ -2,7 +2,7 @@ cask 'barista' do
     version '0.15.2'
     sha256 '6a1dc23d4ce227a5f624bb3764f6e15539a729ab4de37bd69f11a62c0d6116fa'
   
-    url 'https://github.com/moquality/barista-releases/releases/download/v#{version}/Barista-#{version}.dmg'
+    url "https://github.com/moquality/barista-releases/releases/download/v#{version}/Barista-#{version}.dmg"
     name 'Barista'
     homepage 'https://www.moquality.com/'
   
