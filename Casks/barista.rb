4b06d47bae92425474a307fa82494a6d782050e6
Pushkar Kolhe <pushkar@moquality.com>
2018-10-26 04:19:54 -0400

    Updates barista cask to 0.16.18

--- Casks/barista.rb
+++ Casks/barista.rb
@@ -1,6 +1,6 @@
 cask 'barista' do
-    version '0.16.11'
-    sha256 '9355e05bfeedbab43f1b36457ddd45d9eb967149954c7470e9f981df30517e68'
+    version '0.16.18'
+    sha256 '0b1510b380577ed03efe08194a85a10e5da6a0617f944f0114f9add7dbab345a'
 
     url "https://github.com/moquality/barista-releases/releases/download/v#{version}/Barista-#{version}.dmg"
     name 'Barista'
