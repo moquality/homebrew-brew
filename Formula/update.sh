762b45e79d6fa106f0feff032b6bb02d9dd2ebbf
Pushkar Kolhe <pushkar@moquality.com>
2018-12-17 21:40:25 -0500

    Removes sh scripts

--- Formula/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-read -p 'Version: ' VERSION
-if [ ! -z "$VERSION" -a "$VERSION" != " " ]; then
-    wget -nc https://registry.npmjs.org/-/mq-cli-${VERSION}.tgz
-    shasum -a 256 mq-cli-${VERSION}.tgz
-else
-    echo "Version cannot be null"
-fi
--- Formula/upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-read -p 'Version: ' VERSION
-if [ ! -z "$VERSION" -a "$VERSION" != " " ]; then
-    git commit -a -m "Updates mq-cli to ${VERSION}"
-    git push origin master
-fi
